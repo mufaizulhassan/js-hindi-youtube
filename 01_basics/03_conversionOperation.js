71d726ebcd58f8df976dcc5ef9041f2ef9bedf88
MufaizulHassan <154761861+MufaizulHassan@users.noreply.github.com>
2026-05-23 10:10:00 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,17 @@
+let score = undefined
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+// ""=> false 
+// "hitesh"=> false
+
+let someNumber = 33 
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+
+console.log(typeof(stringNumber))
