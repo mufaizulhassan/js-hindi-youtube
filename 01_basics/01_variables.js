3c9827d7a0262b9cd15a072b5560a36a1af2b81b
MufaizulHassan <154761861+MufaizulHassan@users.noreply.github.com>
2026-05-23 09:35:08 +0000

    data_types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountNum ;
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountNum])
---
+++ 01_basics/02_variables.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version 
+
+// alert("hello") // we are using node js nt browser
+
+let state=null // abhi isko value assign hi nhi hui hai 
+
+
+console.log(3+
+    +
+    3)
+
+// number => 2 power 53 
+// bigint better range 
+// stirng =>""
+// boolean =>true / false
+
+//null // standalone value 
+// undefined =>
+// symbol => unique
+
+// object 
+
+console.log(typeof null); // null ek object hai 
+console.log(typeof undefined)
+
+
+
