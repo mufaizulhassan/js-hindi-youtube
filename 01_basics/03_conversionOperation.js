f5c00f75a9f57f909e63cccaa3ffc59d6cab8d66
MufaizulHassan <154761861+MufaizulHassan@users.noreply.github.com>
2026-05-24 10:55:24 +0000

    data type summary

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -15,3 +15,25 @@ let stringNumber = String(someNumber)
 console.log(stringNumber)
 
 console.log(typeof(stringNumber))
+
+
+// ***************** Operations ********************
+
+let value = 3 
+let negative = -value
+console.log(negative)
+
+
+console.log(null>0)
+console.log(null == 0)
+console.log(null >= 0)
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined <0)
+
+// strict chechking 
+
+// check numeric as well as data type 
+
+console.log("2"===2);
---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,19 @@
+// # Primitive 
+
+// 7 types : String , Number , Boolean , null , undefined , Symbol, BigInt(very big values)
+
+
+
+
+
+const id = Symbol('123')
+const anId = Symbol('123')
+console.log(55)
+console.log(id === anId)
+
+// Reference (Non primitive)
+
+// Array, Objects , Functions f
+
+
+
